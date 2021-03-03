861d251cc50f4b70b1a6394881a76f1e1a05c12b
TreasureJim <liam@boreback.com>
2021-03-03 18:46:31 +0800

    data_organiser

--- Assets/explosivevisualisation/data_organiser.py
+++ Assets/explosivevisualisation/data_organiser.py
@@ -1,7 +1,7 @@
 import time
 import pandas as pd
 
-csvFile = "D:\Programming\!Work\AugmentExpertSystems\Explosive Visualiser\Assets\explosivevisualisation/dataset.csv"
+csvFile = r"D:\Programming\!Work\AugmentExpertSystems\Explosive Visualiser\Assets\explosivevisualisation/dataset.csv"
 df = pd.read_csv(csvFile)
 
 # set empty co-ordinate rows as 0 
@@ -17,15 +17,18 @@ df["XF"] = df["XF"] + 1
 df["YF"] = df["YF"] + 1
 df["ZF"] = df["ZF"] + 1
 
+# round all values to remove decimal point
+df[["XC", "YC", "ZC", "XF", "YF", "ZF", "MOVESEQ", "PSTN"]] = df[["XC", "YC", "ZC", "XF", "YF", "ZF", "MOVESEQ", "PSTN"]].round(0).astype(object)
+
 zeroStartBlocks = df[df["MOVESEQ"]==-1] # blocks that dont move
 movingBlocks = df[df["MOVESEQ"]>=0].sort_values("MOVESEQ") # blocks that move
 
-zeroStartBlocks.to_csv("zeroStartBlocks_ts.csv", index=False)
-movingBlocks.to_csv("movingBlocks_ts.csv", index=False)
-df.to_csv("allBlocks_ts.csv")
+zeroStartBlocks.to_csv("zeroStartBlocks.csv", index=False)
+movingBlocks.to_csv("moving_block.csv", index=False)
+df.to_csv("all_block.csv")
 
 print("numTimePoints: ", len(df[df["MOVESEQ"]>=0]))
 print("highest x value: ", max(df.XC)-1, ", highest y value: ", max(df.YC)-1, ", highest z value: ", max(df.ZC)-1)
 print("lowest x value: ", min(df.XC)-1, ", lowest y value: ", min(df.YC)-1, ", lowest z value: ", min(df.ZC)-1)
 
-print(movingBlocks.tail())
+print(df.head(1))
